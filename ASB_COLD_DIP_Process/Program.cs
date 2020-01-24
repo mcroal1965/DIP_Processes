2e1e5aa23e10df4ae8b69c729b96f06400af0ed1
Brent Croal <brent.croal@agile-banking.com>
2020-01-24 07:32:02 -0500

    initial commit; incomplete projects

---
+++ ASB_COLD_DIP_Process/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASB_COLD_DIP_Process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paramfile = args[0];
+            //string client = args[1];
+
+            String ReaderName = null;
+
+            String useDBServer = null;
+            String useDatabase = null;
+            String useTable = null;
+            String useNoteTable = null;
+            String useStatsTable = null;
+            String useInPath = null;
+            String useOutPath = null;
+            String slash = Convert.ToString(Convert.ToChar(92));  //store the slash so it can be used in the filename later
+
+            XmlTextReader reader = new XmlTextReader(paramfile);  // store each line of the input xml file into reader
+
+            while (reader.Read())  //process the rows until no more
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:  //store the name of all node elements into ReaderName
+                        ReaderName = reader.Name;
+                        break;
+                    case XmlNodeType.Text:
+                        if (ReaderName is "DBServer")
+                        { useDBServer = reader.Value; }
+
+                        if (ReaderName is "Database")
+                        { useDatabase = reader.Value; }
+
+                        if (ReaderName is "Table")
+                        { useTable = reader.Value; }
+
+                        if (ReaderName is "NoteTable")
+                        { useNoteTable = reader.Value; }
+
+                        if (ReaderName is "StatsTable")
+                        { useStatsTable = reader.Value; }
+
+                        if (ReaderName is "InPath")
+                        { useInPath = reader.Value; }
+
+                        if (ReaderName is "OutPath")
+                        { useOutPath = reader.Value; }
+                        break;
+
+                }
+            }
+            //Get date items which are the folder names in
+            Int32 datecount = 0;
+            String[] DateItems = Directory.GetDirectories(useInPath, "*.");
+            Int32 numdateitems = DateItems.Count();
+
+            foreach (string dateitem in DateItems)
+            {
+                FileInfo f = new FileInfo(dateitem);
+                String pathdate = f.Name;  //the name of the date folder only eg. 20190512
+                String datepath = dateitem;   //the fullpath of the date folder eg. H:\ASB_COLD\20190512
+                Int32 pathlength = datepath.Length;  //the length of the full path; used to strip that off the filename later
+
+                ++datecount; // increment 
+
+                String Docdate = datepath.Substring(useInPath.Length + 1 + 4, 2) + "/" + datepath.Substring(useInPath.Length + 1 + 6, 2) + "/" + datepath.Substring(useInPath.Length + 1, 4);
+
+                //Get all txt items for date folder
+                String[] txtitems = Directory.GetFiles(datepath, "*.txt");
+                Int32 numtxtitems = txtitems.Count();
+
+                //Get all not items for date folder
+                String[] notitems = Directory.GetFiles(datepath, "*.not", SearchOption.AllDirectories);
+                Int32 numnotitems = notitems.Count();
+
+                Int32 txtcount = 0;
+                Int32 notcount = 0;
+
+                foreach (string txtitem in txtitems)  //txtitem will contain the fullpath to .txt file
+                {
+                    Int32 filenamelength = txtitem.Length - pathlength;  //length of the filename+ext minus the fullpath to it
+                    String filenamextension = Path.GetExtension(txtitem);
+                    Int32 filenamextensionlength = filenamextension.Length + 1;
+                    String filename = txtitem.Substring(pathlength + 1, filenamelength - filenamextensionlength);
+                    String filenamewithextension = filename + filenamextension;
+                    String fullpathfilename = txtitem;
+                    String Unknown = "NO";
+
+                    //Console.WriteLine(txtitem + " | " + itemin + " | " + inx + " | " + ifn + " | ");
+
+                    String RPTnumber = filenamewithextension.Substring(0, filenamewithextension.IndexOf("."));
+                    ++txtcount;
+                    String RPTinstance = filenamewithextension;
+                    Console.Write("Working Date Folder: " + pathdate + " # " + datecount + " of " + numdateitems + " | File: " + filename + " " + txtcount + " of " + numtxtitems + " | Note: " + notcount + " of " + numnotitems + "\n");
+
+                    String sqlCmd = "SELECT TOP 1 a.Application, a.ReportTitle, cast(a.RptRetentionDays as varchar(5)) FROM " + useTable + " a WHERE a.ReportNumber='"+RPTnumber+"'";
+                    String connectionString = "Server=" + useDBServer + ";Database=" + useDatabase + ";User Id=viewer;Password=cprt_hsi";
+                    DataTable dt = new DataTable();  //declare a datable to hold the results
+                    int rowsreturned;
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))  //connect to the sql server
+                    using (SqlCommand cmd = connection.CreateCommand())  //start a  sql command
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))  //use the adapter to get the command results back
+                    {
+                        try
+                        {
+                            cmd.CommandText = sqlCmd;  //set the commandtext to the sqlcmd
+                            cmd.CommandType = CommandType.Text;  //set it as a text command
+                            connection.Open();  //open the sql server connection to the database
+                            rowsreturned = sda.Fill(dt);  //run the command and put the results into the datatable and store the row count
+                            connection.Close();  //close the sql server connection to the database
+
+                            //declare defaults for the report not being in the database
+                            String RPTappl = RPTnumber.Substring(0,3);
+                            String RPTtitle = "Unknown-" + RPTnumber;
+                            String RPTretention = "9999";
+                            Unknown = "YES";
+
+                            if (dt.Rows.Count == 0)
+                            {
+                                //no query results, use defaults
+                            }
+                            else
+                            {
+                                foreach (DataRow drstr in dt.Rows)  //assign each row returned into the variable drstr
+                                {
+                                    string[] values = new string[dt.Columns.Count];  //create a string array with the same number of columns as the datatable (3 in this case)
+                                    
+                                    for (int i = 0; i < dt.Columns.Count; ++i)    //iterate through each column in the datatable for this row
+                                    {
+                                        values[i] = ((string) drstr[i]) ?? "";  //set the value(i) to the column(i) in the datatable but if it is nul ?? then set it to ""
+                                    }
+                                    //use the values from the table as additional keywords in the index file
+                                    RPTappl = values[0];
+                                    RPTtitle = values[1];
+                                    RPTretention = values[2];
+                                    Unknown = "NO";
+                                }
+                            }
+                            //Calculate Retention End Date keyword for use in workflow to delete when exceeded
+                            String RED = "";
+                            if (RPTretention == "0")
+                            {
+                                //0 so no retention date;permanent record
+                            }
+                            else
+                            {
+                                DateTime Docdate2 = Convert.ToDateTime(Docdate);
+                                DateTime RetentionEndDate = Docdate2.AddDays(Convert.ToInt32(RPTretention));
+                                RED = RetentionEndDate.ToString("MM/dd/yyyy");
+                            }
+                        
+                            String Desc=RPTnumber+" - "+RPTtitle;  //Description keyword part of autoname string
+
+                            String outDIPindexfile = "DIPindex_"+ pathdate +"_"+ filename + ".txt".Replace(" ", "");  //the name of the index file to be used for this report txt file
+                            String outCOPYfile = pathdate + "_" + filenamewithextension.Replace(" ", "");  //the name of the file prefixed with the folder date it is in to handle multiple report files for same day
+
+                            String DIPIndexValue = "ASB Report Legacy\t" + Docdate + "\t" + RPTinstance + "\t" + RPTnumber + "\t" + RPTappl + "\t" + RPTtitle + "\t" + RED + "\t" + Desc + "\t" + useOutPath + slash +outCOPYfile;
+
+                            //update the report control table if the report number was unknown so the next time we run into it will have values
+                            if (Unknown == "YES")
+                            {
+                                sqlCmd = "Insert Into " + useTable + " ([Application],[ReportNumber],[ReportTitle],[RPTretentionDays]) values ('" + RPTappl + "', '" + RPTnumber + "', '" + RPTtitle + "', '"+ RPTretention + "')";
+                                using (SqlConnection connection2 = new SqlConnection(connectionString))  //connect to the sql server
+                                using (SqlCommand cmd2 = connection2.CreateCommand())  //start a  sql command
+                                    try
+                                    {
+                                        cmd2.CommandText = sqlCmd;  //set the commandtext to the sqlcmd
+                                        cmd2.CommandType = CommandType.Text;  //set it as a text command
+                                        connection2.Open();  //open the sql server connection to the database
+                                        int rowsadded = cmd2.ExecuteNonQuery();  //run the command and store the row count inserted
+                                        connection2.Close();  //close the sql server connection to the database
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine("Error: " + ex);
+                                    }
+                            }
+                            //now that we have created the index values for this report, write the file out, copy the report from the temp folder to the DIP folder
+                            File.WriteAllText(useOutPath+slash+outDIPindexfile, DIPIndexValue);
+                            File.Copy(fullpathfilename, useOutPath + slash + outCOPYfile, true);
+                            //File.Delete(fullpathfilename);
+
+                            //*HANDLE THE .NOT FILES HERE
+
+
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine("Error: "+ex);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ InstantOpen_DIP_Extract/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace InstantOpen_DIP_Extract
+{
+    class Program
+    {
+        public static string mappeddoctype { get; internal set; }
+        
+
+        static void Main(string[] args)
+        {
+            string paramfile = args[0];
+            //string client = args[1];
+
+            String ReaderName = null;
+
+            String useDBServer = null;
+            String useDatabase = null;
+            String useTable = null;
+            String useXMLTable = null;
+            String useInPath = null;
+            String useOutPath = null;
+            String useDrivePath = null;
+
+            String slash = Convert.ToString(Convert.ToChar(92));  //store the slash so it can be used in the filename later
+
+            XmlTextReader reader = new XmlTextReader(paramfile);  // store each line of the input xml file into reader
+
+            while (reader.Read())  //process the rows until no more
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:  //store the name of all node elements into ReaderName
+                        ReaderName = reader.Name;
+                        break;
+                    case XmlNodeType.Text:
+                        if (ReaderName is "DBServer")
+                        { useDBServer = reader.Value; }
+
+                        if (ReaderName is "Database")
+                        { useDatabase = reader.Value; }
+
+                        if (ReaderName is "Table")
+                        { useTable = reader.Value; }
+
+                        if (ReaderName is "XMLTable")
+                        { useXMLTable = reader.Value; }
+
+                        if (ReaderName is "InPath")
+                        { useInPath = reader.Value; }
+
+                        if (ReaderName is "OutPath")
+                        { useOutPath = reader.Value; }
+                        
+                        if (ReaderName is "DrivePath")
+                        { useDrivePath = reader.Value; }
+                        break;
+                }
+            }
+            //Get all items in all folder and subfolders
+            String[] allitems = Directory.GetFiles(useInPath, "*.*", SearchOption.AllDirectories);
+            Int32 numitems = allitems.Count();
+            Int32 pathlength = useInPath.Length;
+            String Docdate;
+            Int32 workingitemnum = 0;
+
+            foreach (string item in allitems)
+            {
+                ++workingitemnum;
+                Console.WriteLine("Working: " + workingitemnum + " of " + numitems);
+                FileInfo f = new FileInfo(item);
+                Int32 filenamelength = item.Length - pathlength;  //length of the filename+ext minus the fullpath to it
+                String filenamextension = Path.GetExtension(item);
+                Int32 filenamextensionlength = filenamextension.Length + 1;
+                String filename = item.Substring(pathlength + 1, filenamelength - filenamextensionlength);
+                String filenamewithextension = filename + filenamextension;
+                String fullpathfilename = item;
+
+                //handle the not xml files first
+                if (filenamextension != ".xml")
+                {
+                    string[] splittext = filename.Split("_");
+                    String custname = splittext[0];
+                    String ssn = splittext[1];
+                    String acctnum = splittext[2];
+                    String tranid = splittext[3];
+                    String doctype = splittext[4];
+                    
+                    String docdate = splittext[5];
+                    Docdate = docdate.Substring(0, 2) + "/" + docdate.Substring(2, 2) + "/" + docdate.Substring(4, 4);
+                    String sqlCmd = "SELECT TOP 1 a.NautilusDoctype FROM  " + useTable + " a WHERE a.OnlineBankingDoctype='" + doctype + "'";
+                    String connectionString = "Server=" + useDBServer + ";Database=" + useDatabase + ";User Id=viewer;Password=cprt_hsi";
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))  //connect to the sql server
+                    using (SqlCommand cmd = connection.CreateCommand())  //start a  sql command
+                    {
+                        try
+                        {
+                            //see if the document name extracted from the filename is in the mapping table
+                            cmd.CommandText = sqlCmd;  //set the commandtext to the sqlcmd
+                            cmd.CommandType = CommandType.Text;  //set it as a text command
+                            connection.Open();  //open the sql server connection to the database
+                            var dbreader = cmd.ExecuteReader();  //run the command and put the results into dbreader
+                            
+                            //if the reader has rows get the mapper doctype from the table
+                            while (dbreader.Read())
+                            {
+                                string NautilusDocType = dbreader.GetValue(dbreader.GetOrdinal("NautilusDoctype")).ToString();
+                                mappeddoctype = NautilusDocType;
+                            }
+                            connection.Close();  //close the sql server connection to the database
+                            //default this keyword to nil because if the document mapping doesnt exist we'll put the name from the file into description kw 
+                            //for DIP because these will map to DEP Disclosure and the workflow will assign Description to the TYP kw
+                            String Description = "";
+                            String DIPDoctype = "";
+                            
+                            if (mappeddoctype == "")
+                            {
+                                DIPDoctype = "DEP Disclosure";
+                                Description = doctype;
+                            }
+                            else
+                            {
+                                DIPDoctype = mappeddoctype;
+                            }
+
+                            String outDIPindexfile = "DIPindex_" + "_" + filename + ".txt".Replace(" ", "");  //the name of the index file to be used for this file
+
+                            //build the line for the index file
+                            String DIPIndexValue = DIPDoctype + "\t" + Docdate + "\t" + acctnum + "\t" + custname + "\t" + ssn + "\t" + tranid + "\t" + Description + "\t" + useDrivePath + slash + filenamewithextension;
+
+                            File.WriteAllText(useOutPath + slash + outDIPindexfile, DIPIndexValue);
+                            File.Copy(fullpathfilename, useOutPath+slash+filenamewithextension, true);
+        //                    File.Delete(fullpathfilename);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error: " + ex);
+                        }
+                    }
+                }
+                //handle the xml files
+                if (filenamextension == ".xml")
+                {
+
+                }
+        
+            }
+        }
+    }
+}
